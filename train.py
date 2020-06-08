1a81206d90744f8b7ab521c44a7dd414690139cf
Jaison Jose <jaisonjose241@gmail.com>
2020-06-08 17:47:14 +0530

    train python file

--- train.py
+++ train.py
@@ -44,6 +44,7 @@ classifier.add(Dropout(0.2))
 classifier.add(Flatten())
 # Adding a fully connected layer
 classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=64, activation='relu'))
 classifier.add(Dense(units=6, activation='softmax')) 
 
 # Compiling the CNN
@@ -55,32 +56,34 @@ classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['
 train_datagen = ImageDataGenerator(
         rescale=1./255,
         shear_range=0.2,
-        zoom_range=0.2)
+        zoom_range=0.2,
+        horizontal_flip=True,
+        rotation_range=10)
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory('data/TRAIN',
                                                  target_size=(128, 128),
-                                                 batch_size=10,
+                                                 batch_size=20,
                                                  color_mode='grayscale',
                                                  class_mode='categorical')
 
 test_set = test_datagen.flow_from_directory('data/TEST',
                                             target_size=(128, 128),
-                                            batch_size=10,
+                                            batch_size=20,
                                             color_mode='grayscale',
                                             class_mode='categorical') 
 classifier.fit_generator(
         training_set,
-        steps_per_epoch=600, # No of images in training set
-        epochs=70,
+        steps_per_epoch=500, # No of images in training set
+        epochs=40,
         validation_data=test_set,
         validation_steps=10)# No of images in test set
 
 
 # Saving the model
 model_json = classifier.to_json()
-with open("model-bw.json", "w") as json_file:
+with open("model-sm.json", "w") as json_file:
     json_file.write(model_json)
-classifier.save_weights('model-bw.h5')
+classifier.save_weights('model-sm.h5')
 
